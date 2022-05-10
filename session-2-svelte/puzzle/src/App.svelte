bcd53286b553137e860b5900be6ebf7a71dedcb3
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-05-10 15:41:11 -0700

    created puzzle

--- session-2-svelte/puzzle/src/App.svelte
+++ session-2-svelte/puzzle/src/App.svelte
@@ -1,11 +1,9 @@
 <script>
 	let name = "Jakob";
 	let src = "./images/mom-image.jpg";
-	let columns = 14;
-	let rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-	let availableTickets = [];
-	let i;
-	let j;
+	let columns;
+	let rows = [];
+	let availableTickets;
 	let seat;
 	let chosenSeats = [];
 	for(i = 0; i < rows.length; i++)
@@ -13,47 +11,39 @@
 		for(j = 0; j < columns; j++)
 		{
 			seat = rows[i] + `${j}`;
-			availableTickets.push(seat);
 		}
 	}
-	let snacks = [
-		{ id: 1, text: `Large Popcorn`, count: 0 },
-		{ id: 2, text: `Icee`, count: 0 },
-		{ id: 3, text: `Vanilla Ice Cream Scoop`, count: 0 },
-		{ id: 4, text: `Baked Pretzel`, count: 0 },
-		{ id: 5, text: `Mike N Ikes Pack`, count: 0 }
-	];
+	let snacks = [];
 	let selected;
 	let answer = '';
 
 	function handleSubmit() {
 		snacks[selected.id - 1].count += answer;
-		alert(`Purchased ${answer} ${selected.text}(s)`);
 	}
 </script>
 
 <main>
 	<div>
-		<h1>Doctor Strange in the Multiverse of Madness</h1>
+		<h1>Doctor Strange in the Wandaverse of Madness</h1>
 		<img {src} alt="Multiverse of Madness Image">
-		<h2>Click the tickets you want to buy! </h2>
+		<h2>Click on thy tickets please maybe possibly</h2>
 	</div>
-	<h1>*Screen*</h1>
+	<h1>*Scream*</h1>
 	{#each rows as { row }, i}
 		<div>
 			{#each Array(columns) as _, column}
 				<label>
-					<input type=checkbox bind:group={chosenSeats} value={availableTickets[i * 14 + column]}>
+					<input type=checkbox value={availableTickets[i * 14 + column]}>
 					{availableTickets[i * 14 + column]} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				</label>
 			{/each}
 		</div>
 	{/each}
-	<h2>Currently, {chosenSeats.length} tickets have been purchased. There are {112 - chosenSeats.length} open seats.</h2>
-	<h2>Snack Order</h2>
+	<h2>Currently, x amount of tickets have been purchased. There are {chosenSeats.length} open seats.</h2>
+	<h2>Snacc Order</h2>
 
-	<form on:submit|preventDefault={handleSubmit}>
-		<select bind:value={selected} on:change="{() => answer = ''}">
+	<form on:submit|preventDefault={}>
+		<select value={selected} on:change="{() => answer = ''}">
 			{#each snacks as snack}
 				<option value={snack}>
 					{snack.text}
@@ -61,23 +51,18 @@
 			{/each}
 		</select>
 
-	<h4>How many?</h4>
+	<h4>How many snaccccs?</h4>
 
-	<input type="number" bind:value={answer}>
+	<input type="number" value={answer}>
 
-	<button disabled={!answer} type=submit>
-		Submit
+	<button type=submit>
+		GO!
 	</button>
 
 	<p>Selected {selected ? answer + ' ' + selected.text + '(s)' : '[waiting...]'}</p>
 
-	<h1>Total Sales: </h1>
-
 	<ul>
-		<li>Tickets: {chosenSeats.length}</li>
-		{#each snacks as snack}
-			<li>{snack.text}: {snack.count}</li>
-		{/each}
+		<li>Tix: {chosenSeats.length}</li>
 	</ul>
 </main>
 
